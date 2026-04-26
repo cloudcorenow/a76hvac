9b8e4f8ad15b801c10f0e87f20404643a198bc09
cloudcorenow <138939871+cloudcorenow@users.noreply.github.com>
2026-04-26 15:08:45 -0700

    Updated Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -49,9 +49,9 @@ export default function Footer({ onNavigate }: FooterProps) {
           <h4>Contact</h4>
           <ul>
             <li>
-              <a href="tel:8007760076">
+              <a href="tel:7147409165">
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07A19.5 19.5 0 013.07 9.81a19.79 19.79 0 01-3.07-8.67A2 2 0 012 .84h3a2 2 0 012 1.72c.127.96.361 1.903.7 2.81a2 2 0 01-.45 2.11L6.09 8.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0122 16.92z"/></svg>
-                (800) 776-0076
+                (714) 740-9165
               </a>
             </li>
             <li>
