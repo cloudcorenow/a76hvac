4a0d2677318e7ff6debfd5bcf5f060ea29ab4bac
cloudcorenow <138939871+cloudcorenow@users.noreply.github.com>
2026-04-26 17:34:19 -0700

    Adjust logo position in nav & footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,7 +20,7 @@ export default function Footer({ onNavigate }: FooterProps) {
             <img
               src="https://imagedelivery.net/s0JEtwqnLquT1GUYjPcg5Q/c1245cc8-766a-4709-c140-14e14c610d00/public"
               alt="Allegiance 76 Heating and Air Conditioning LLC"
-              style={{ height: 110 }}
+              style={{ height: 130 }}
             />
             <span>Allegiance 76 Heating and Air Conditioning LLC</span>
           </a>
